40f6426687b71a8db44786ffdbfaa94322c8c122
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-08-22 18:40:42 +0200

    image cache service

--- Image Gallery/Data/ImageCacheService.swift
+++ Image Gallery/Data/ImageCacheService.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+actor ImageCacheService {
+  private let cache = NSCache<NSString, NSData>()
+  
+  func getImage(for key: String) -> Data? {
+    cache.object(forKey: key as NSString) as? Data
+  }
+  
+  func setImage(_ data: Data, for key: String) {
+    cache.setObject(data as NSData, forKey: key as NSString)
+  }
+}
