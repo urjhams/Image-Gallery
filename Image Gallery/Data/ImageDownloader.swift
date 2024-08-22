c8d6aba35342e474634f6175457ebe80987fb05b
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-08-22 18:35:49 +0200

    add error file, image downloader

--- Image Gallery/Data/ImageDownloader.swift
+++ Image Gallery/Data/ImageDownloader.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+actor ImageDownloader {
+  func downloadImage(from url: URL) async throws -> Data {
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+      throw NetworkError.invalidResponse
+    }
+    
+    return data
+  }
+}
